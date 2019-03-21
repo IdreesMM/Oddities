4a666c1df0c324e7b3e3074f76b5934aa66f8a33
IdreesMM <imirani@iu.edu>
2019-03-21 14:32:59 -0400

    123

---
+++ oddities.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class oddities{
+	
+	public static void main (String[]args){
+	
+		Scanner input = new Scanner(System.in);
+		
+		int testcase = args[0]; 
+		
+		System.outprintln("Choose number of number you would like to input: ");		
+
+		for(int i = 0; i < testCase; i++){
+		
+			int numberToCheck = input.nextInt();
+			
+			boolean isEven = false;
+		}//for
+
+		if(numberToCheck%2 == 1){
+	
+			boolean isEven = false;	
+	
+		}//else if
+
+		else{
+			
+			boolean isEven = true;
+ 			
+		}//else
+
+	}//main
+
+}//oddities
